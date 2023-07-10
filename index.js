48ed9dc15fb250ad764226c4b25adf067d354ecb
Nathan <nathan.wendlowsky@flatironschool.com>
2023-07-10 14:00:33 -0700

    Another prework lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, type) {
+    const thankyouMessages = [];
+    for (let i =0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${type} gift!`;
+        thankyouMessages.push(message);
+    }
+    return thankyouMessages;
+
+}
+writeCards(names,'surprise');
+
+function countDown(integer) {
+    let number = 0;
+    while (number <= integer) {
+        console.log(integer--);
+    }
+}
+countDown(10);
